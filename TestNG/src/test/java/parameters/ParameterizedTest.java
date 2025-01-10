9c734798ba4cb3fbb72554700653aa5bd6f7ac43
ADMIN <ADMIN@Ranjith>
2025-01-10 09:46:42 +0530

    Initial Commit

--- TestNG/src/test/java/parameters/ParameterizedTest.java
+++ TestNG/src/test/java/parameters/ParameterizedTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
